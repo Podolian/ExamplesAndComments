0418e0c0fe853e7721c6329b81817354ab1efe1b
Podolian <demian.podolian@gmail.com>
2018-08-16 12:26:57 +0300

    2 things in main: array index and parralel strem

---
+++ array-index/src/main/java/com/dp/IndexExample.java
@@ -0,0 +1,28 @@
+package com.dp;
+
+import java.util.Arrays;
+
+public class IndexExample {
+
+    /**
+     * This is just to show thar it's possible to refer array element by index right after
+     * calling {@Link String} method.
+     * And the same, but using parallel stream.
+    */
+    private static String partOfStringByIndex(int arrayIndex, String anyString) {
+        return anyString.split(" ")[arrayIndex];
+    }
+
+    public static void main(String[] args) {
+        String numeros = "uno dos tres";
+
+        for(int i = 0; i<3; i++){
+            System.out.println(partOfStringByIndex(i, numeros));
+        }
+
+
+        Arrays.stream(numeros.split(" "))
+                .parallel()
+                .forEach(System.out::println);
+    }
+}
